e826c8dd6fcd05c45860bd5cfaa58bb1f95f0db2
Jodi Pranata <jodranata@gmail.com>
2019-12-29 21:49:07 +0700

    Updates

---
+++ precache-manifest.ec2750c10cb382aacf41a36c6bef66bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5602b37c934f93337f5a3f540f7d0749",
+    "url": "/react-hackerranknews/index.html"
+  },
+  {
+    "revision": "bfb4682f28368e93134e",
+    "url": "/react-hackerranknews/static/css/main.b5439b80.chunk.css"
+  },
+  {
+    "revision": "614255b5346ba1c5e3a4",
+    "url": "/react-hackerranknews/static/js/2.965d74b8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-hackerranknews/static/js/2.965d74b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfb4682f28368e93134e",
+    "url": "/react-hackerranknews/static/js/main.27f0c6d7.chunk.js"
+  },
+  {
+    "revision": "4f05ff3c04fe4c2034b4",
+    "url": "/react-hackerranknews/static/js/runtime-main.95391251.js"
+  }
+]);
